3afb70bda44b68bb76e58b067f1226111d5a0ebc
Anastasia Dmitrieva <dmitrieva.adm@students.dvfu.ru>
2024-03-03 16:25:02 +1000

    Laba 1

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <span class="hello">Hello,</span><span class="world"> world!</span>
+  </div>
+</template>
+  
+<script>
+  console.log("Hello, world!")
+</script>
+
+<style>
+  @import "src/assets/main.css";
+</style>
