0240701d2c25595584ef53ccaf86b65ae80129be
Anastasia Dmitrieva <dmitrieva.adm@students.dvfu.ru>
2024-03-10 21:34:17 +1000

    Laba 2

--- app.vue
+++ app.vue
@@ -1,13 +1,20 @@
-<template>
-  <div>
-    <span class="hello">Hello,</span><span class="world"> world!</span>
-  </div>
-</template>
-  
 <script>
-  console.log("Hello, world!")
+  import Header from '~/components/Header.vue';
+  import Content from '~/components/Content.vue';
+  import Footer from '~/components/Footer.vue';
 </script>
 
-<style>
-  @import "src/assets/main.css";
+<template>
+  <Header />
+  <Content />
+  <Footer />
+</template>
+  
+<style lang="scss">
+:root {
+   --font-title: Montserrat, sans-serif;
+   --font-text: Open Sans, sans-serif;
+
+   font-family: var(--font-text);
+}
 </style>
---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <div class="footer">
+        <p class="footer-text">Footer</p>
+    </div>
+</template>
+
+<style lang="scss">
+.footer {
+    width: 100%;
+    height: 70px;
+    background-color: #808080;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    padding-left: 30px;
+    padding-top: 10px;
+}
+</style>
